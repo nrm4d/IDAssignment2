6a4427326ad4ae541203cd363206c4568993a02f
nrm4d <s10227737@connect.np.edu.sg>
2022-02-03 16:04:56 +0800

    added more stuff for api

--- api.js
+++ api.js
@@ -1,4 +1,33 @@
 import {MY_API_KEY} from 'config.js';
+let covid19data;
+
+(function onLoad()
+{
+    // set a function for each button
+    setButtonFunctions();
+	getLatestCOVID19Data();
+})();
+function setButtonFunctions()
+{
+    
+    document.getElementById('countries').onchange = function() {
+        const selectedValue = document.getElementById('countries').value;
+        const countryData = covid19data.filter(c => c.country == selectedValue)[0];
+
+        // display data
+        const newConfirmed = document.getElementById('covidNewConfirmed');
+        const totalConfirmed = document.getElementById('covidTotalConfirmed');
+        const covidNewDeaths = document.getElementById('covidNewDeaths');
+        const covidTotalDeaths = document.getElementById('covidTotalDeaths');
+        const lastUpdated = document.getElementById('covidLastUpdate');
+
+        (countryData.cases.new) ? newConfirmed.innerHTML = 'New confirmed cases: ' + countryData.cases.new : newConfirmed.innerHTML = 'New confirmed cases: 0';
+        (countryData.cases.total) ? totalConfirmed.innerHTML = 'Total confirmed cases: ' + countryData.cases.total : totalConfirmed.innerHTML = 'Total confirmed cases: 0';
+        (countryData.deaths.new) ? covidNewDeaths.innerHTML = 'New deaths: ' + countryData.deaths.new : covidNewDeaths.innerHTML = 'New deaths: 0';
+        (countryData.deaths.total) ? covidTotalDeaths.innerHTML = 'Total deaths: ' + countryData.deaths.total : covidTotalDeaths.innerHTML = 'Total deaths: 0';
+        lastUpdated.innerHTML = 'Last updated: ' + countryData.day;
+    };
+}
 
 async function getLatestCOVID19Data()
 {
