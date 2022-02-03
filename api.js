92a59c0ddc880de3084301c16e5740fa598bafe7
nrm4d <s10227737@connect.np.edu.sg>
2022-02-03 15:56:02 +0800

    extra security for api key maybe idk

--- api.js
+++ api.js
@@ -1,13 +1,31 @@
-fetch("https://covid-19-data.p.rapidapi.com/country?name=italy", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
-		"x-rapidapi-key": "3621becfedmsh51a35fc00389fa5p163fbcjsna0e34515b714"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+import {MY_API_KEY} from 'config.js';
+
+async function getLatestCOVID19Data()
+{
+    await fetch("https://covid-193.p.rapidapi.com/statistics", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "covid-193.p.rapidapi.com",
+            "x-rapidapi-key": MY_API_KEY
+        }
+    })
+    .then(response => response.json())
+    .then(response => {
+        console.log("COVID 19 API object:");
+        console.log(response);
+        console.log("\n");
+
+        // add all countries to select element
+        response.response.forEach(c => {
+            const option = document.createElement('option');
+            option.innerHTML = c.country;
+            document.getElementById('countries').appendChild(option);
+        })
+
+        // save covid data to global variable
+        covid19data = response.response;
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
---
+++ config.js
@@ -0,0 +1,2 @@
+const MY_API_KEY = 'c0af6827cemsh249ad0ac2f4087fp19ea60jsnbc5e03ecd889';
+export {MY_API_KEY};
