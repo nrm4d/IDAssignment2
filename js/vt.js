e62d67a9c63ae938dd55103ddda34855f1558b01
daryl <Your own email>
2022-02-13 22:03:12 +0800

    changed the contact form

--- js/vt.js
+++ js/vt.js
@@ -1,4 +1,4 @@
 const inputdate = document.getElementById('date')
 var onemonth = 30;
 var nextdose = inputdate.setDate(inputdate.getDate() + onemonth);
-messages.push(nextdose);
+messages.push('The next date to your next vaccination dose is:' + nextdose);
