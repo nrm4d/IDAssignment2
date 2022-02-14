8e10e0bd93c23a19e520ceb647fc3122fab27c97
daryl <Your own email>
2022-02-14 22:56:29 +0800

    continued vaccination tracker

--- js/vt.js
+++ js/vt.js
@@ -1,4 +1,14 @@
-const inputdate = document.getElementById('date')
-var onemonth = 30;
-var nextdose = inputdate.setDate(inputdate.getDate() + onemonth);
-messages.push('The next date to your next vaccination dose is:' + nextdose);
+$(document).ready(function () {
+var choosedate= "";
+$("#submit").on("click", function(e){
+    e.preventDefault();
+    myfunction();
+  })
+function myfunction(){
+    const inputdate = document.getElementById('date');
+    choosedate= new Date(inputdate.value);
+    choosedate = choosedate.getDate() + 30;
+    var futureDate = chooseDate.getFullYear()+'-'+('0'+(chooseDate.getMonth()+1)).slice(-2)+'-'+('0'+(chooseDate.getDate())).slice(-2);
+    document.getElementById("nextdose").innerHTML = ('your next dose is :  '+futureDate);
+}
+})
