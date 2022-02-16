cbbc38df464ce5c527e1a8b7390ed0ce76d47a14
daryl <Your own email>
2022-02-16 22:34:47 +0800

    finish vaccination tracker

--- js/vt.js
+++ js/vt.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
 var choosedate= "";
-$("#submit").on("click", function(e){
+$("#nextdate").on("click", function(e){
+  console.log("hi");
     e.preventDefault();
     myfunction();
   })
 function myfunction(){
-    const inputdate = document.getElementById('date');
-    choosedate= new Date(inputdate.value);
-    choosedate = choosedate.getDate() + 30;
-    var futureDate = chooseDate.getFullYear()+'-'+('0'+(chooseDate.getMonth()+1)).slice(-2)+'-'+('0'+(chooseDate.getDate())).slice(-2);
-    document.getElementById("nextdose").innerHTML = ('your next dose is :  '+futureDate);
+    choosedate= new Date(document.getElementById('date').value);
+    choosedate.setDate(choosedate.getDate() + 30);
+    console.log(choosedate);
+    document.getElementById("nextdose").innerHTML = ('your next dose is :  '+ choosedate);
 }
 })
